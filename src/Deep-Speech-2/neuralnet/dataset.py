bd94928b083a0f1bf1d34c192c8c301b9e84e27f
luluw <shashinmhrzn@gmail.com>
2024-11-02 14:46:03 +0545

    :rocket: [Update] Changes on featurizer

--- src/Deep-Speech-2/neuralnet/dataset.py
+++ src/Deep-Speech-2/neuralnet/dataset.py
@@ -8,26 +8,20 @@ from torch.utils.data import DataLoader, Dataset
 from utils import TextTransform
 
 class LogMelSpec(nn.Module):
-    def __init__(self, sample_rate=16000, n_mels=80, win_length=400, hop_length=160, n_fft=512):
+    def __init__(self, sample_rate=16000, n_mels=80, hop_length=160):
         super(LogMelSpec, self).__init__()
         self.transform = transforms.MelSpectrogram(sample_rate=sample_rate, 
                                                    n_mels=n_mels,
-                                                   win_length=win_length,
-                                                   hop_length=hop_length, 
-                                                   n_fft = n_fft)
+                                                   hop_length=hop_length)
 
     def forward(self, x):
-        x = self.transform(x)
-        # x = torch.log(x + 1e-14)  # logarithmic, add small value to avoid inf
-        return x
+        return self.transform(x)
 
 
-def get_featurizer(sample_rate=16000, n_mels=80, win_length=400, hop_length=160, n_fft=512):
+def get_featurizer(sample_rate=16000, n_mels=80, hop_length=160):
     return LogMelSpec(sample_rate=sample_rate, 
                       n_mels=n_mels,
-                      win_length=win_length,
-                      hop_length=hop_length, 
-                      n_fft = n_fft)
+                      hop_length=hop_length)
 
 
 class CustomAudioDataset(Dataset):
