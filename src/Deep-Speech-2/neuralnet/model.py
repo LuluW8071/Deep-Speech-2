f53057896c8a44ea85eaeea952543667b9e16947
luluw <shashinmhrzn@gmail.com>
2024-11-04 14:47:49 +0545

    :rocket: [ADD] minGRU Implementation with Speech Recognition

--- src/Deep-Speech-2/neuralnet/model.py
+++ src/Deep-Speech-2/neuralnet/model.py
@@ -45,3 +45,56 @@ class SpeechRecognitionModel(nn.Module):
         return x
     
 
+""" Speech Recognition with minGRU """
+class SpeechRecognition_minGRUModel(nn.Module):
+    def __init__(self, n_cnn_layers, n_rnn_layers, rnn_dim, n_class, n_feats, stride=2, dropout=0.1):
+        super(SpeechRecognition_minGRUModel, self).__init__()
+        n_feats = n_feats//2
+        self.cnn = nn.Conv2d(1, 32, 3, stride=stride, padding=3//2)  # cnn for extracting heirachal features
+
+        # n residual cnn layers with filter size of 32
+        self.rescnn_layers = nn.Sequential(*[
+            ResidualCNN(32, 32, kernel=3, stride=1, dropout=dropout, n_feats=n_feats) 
+            for _ in range(n_cnn_layers)
+        ])
+        self.fully_connected = nn.Linear(n_feats*32, rnn_dim)
+        self.birnn_layers = nn.Sequential(*[
+            MinGRU(dim=rnn_dim,
+                   expansion_factor=1.0 if i==0 else 2.0,
+                   dropout_prob=dropout)
+            for i in range(n_rnn_layers)
+        ])
+        self.classifier = nn.Sequential(
+            nn.Linear(rnn_dim, rnn_dim),  # birnn returns rnn_dim*2
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(rnn_dim, n_class)
+        )
+
+    def forward(self, x):
+        x = self.cnn(x)
+        x = self.rescnn_layers(x)
+        sizes = x.size()
+        x = x.view(sizes[0], sizes[1] * sizes[2], sizes[3])  
+        x = x.transpose(1, 2)                           
+        x = self.fully_connected(x)
+        print(x.shape)
+        x = self.birnn_layers(x)
+        print(x.shape)
+        x = self.classifier(x)
+        return x
+
+if __name__ == "__main__":
+    x = torch.randn(1, 1, 80, 128)  
+    model2 = SpeechRecognition_minGRUModel(n_cnn_layers=2,
+                                          n_rnn_layers=3,
+                                          rnn_dim=512, 
+                                          n_class=29, 
+                                          n_feats=80, 
+                                          stride=2, 
+                                          dropout=0.1)
+    print(model2)
+    out2 = model2(x)
+
+    print("Output 2:", out2)
+    print(out2.shape)
